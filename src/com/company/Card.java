0fa8851436109b1e4adc3e6d407673a6f0397547
Naagibator228 <taranmarya@yandex.ru>
2016-11-11 19:16:05 +0300

    Initial commit

---
+++ src/com/company/Card.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by student2 on 11.11.16.
+ */
+public class Card {
+    Suit suit;
+    Value value;
+
+    @Override
+    public String toString() {
+        return ""+ value +" OF "+ suit;
+
+    }
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+}
